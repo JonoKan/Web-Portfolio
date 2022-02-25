918b55cb03826aea9c7998124c4f9f445df0a292
JonoKan2001 <79693698+JonoKan2001@users.noreply.github.com>
2022-02-25 18:00:41 +1300

    Added working animation for switching open menu

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const projectDets = {
     text:"Ever wanted to be unproductive? This Chrome extension was made during a hackathon with a group 6 other friends. The extension changes the default curson into a duck we designed. The initial click on a website deemed productive changes the duck into an angry duck which will grow with each click until the duck covers the screen. On unproductive sites the cursor will change to a happy duck and will grow smaller until the default size. Making this extension was extremely fun and beneficial as it helped me to begin understanding JavaScript and APIs and also helped me to network with fellow Computer Science and Software Engineering student"
     }
 }
+
 const showMore = (project) => {
     const prjImg = document.getElementById('project-image');
     const prjLang = document.getElementById('project-languages');
@@ -27,4 +28,14 @@ const showMore = (project) => {
         prjLang.appendChild(node);
     });
     prjDesc.innerHTML = projectDets[project].text;
+}
+
+const myFunction = () => {
+  document.getElementById('menu-button').classList.toggle('toggle-on');
+  var toggleMenu = document.getElementsByClassName('toggle-menu');
+  if (toggleMenu.style.display === "none") {
+    toggleMenu.style.display = "flex";
+  } else {
+    toggleMenu.style.display = "none";
+  }
 }
