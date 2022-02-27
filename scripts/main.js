6b0e6c80b1e2cfba466cec9ea74a85d78ed90260
JonoKan2001 <79693698+JonoKan2001@users.noreply.github.com>
2022-02-28 00:21:49 +1300

    Renamed script files

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
+const toggleMenu = document.querySelector('.toggle-menu');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
+    toggleMenu.classList.toggle('is-active');
+
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
+    toggleMenu.classList.toggle('is-active');
   }
 }); 
--- scripts/viewFullProect.js
+++ scripts/viewFullProect.js
@@ -29,5 +29,3 @@ const showMore = (project) => {
     });
     prjDesc.innerHTML = projectDets[project].text;
 }
-
-
