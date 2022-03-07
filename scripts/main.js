d5fd85fd00746f3733a33a71e250fd47a1136b2d
JonoKan2001 <74647687+JonoKan@users.noreply.github.com>
2022-03-07 23:42:24 +1300

    Removed unused ID tag from pages. Moved menu to right

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 const menuBtn = document.querySelector('.menu-btn');
 const toggleMenu = document.querySelector('.toggle-menu');
+const mediaQuery = window.matchMedia('(min-width: 768px)')
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
     toggleMenu.classList.toggle('is-active');
-
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
     toggleMenu.classList.toggle('is-active');
+  }
+  if (toggleMenu.classList.contains('is-active') && mediaQuery.matches){
+   toggleMenu.classList.toggle('is-active');
   }
 }); 
